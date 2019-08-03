b6a1c9cfd614e3bcf5dd45ba54de79898fa290a4
Aria Kim <shoo7830@gmail.com>
2019-08-03 23:32:42 +0900

    middleware

--- src/store.js
+++ src/store.js
@@ -1,6 +1,10 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import modules from './modules';
 
-const store = createStore(modules)
+import {createLogger} from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
+
+const logger = createLogger();
+const store = createStore(modules, applyMiddleware(logger, ReduxThunk))
 
 export default store;
